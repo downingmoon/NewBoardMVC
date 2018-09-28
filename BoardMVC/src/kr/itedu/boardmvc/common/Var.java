d7aa4b14aad65680ed6473896b6b6aa724bdb132
USER <USER@USER18-PC>
2018-09-28 10:22:34 +0900

    boardMVC

---
+++ BoardMVC/src/kr/itedu/boardmvc/common/Var.java
@@ -0,0 +1,13 @@
+package kr.itedu.boardmvc.common;
+
+public class Var {
+	
+	public static final String TEMPLATE_1 = "jsp/template.jsp";
+	
+	//public static final String DETAIL = "jsp/boardDetail.jsp";
+	
+	public static final String[] TITLES = { "공지", "자유", "유머", "btype3" 	};
+	
+	public static final int boardItemCount = 10;
+
+}
---
+++ BoardMVC/src/kr/itedu/boardmvc/service/BoardListService.java
@@ -0,0 +1,25 @@
+package kr.itedu.boardmvc.service;
+
+import java.util.ArrayList;
+
+import kr.itedu.boardmvc.BoardVO;
+import kr.itedu.boardmvc.common.BoardDAO;
+
+public class BoardListService {
+	
+	public int getBoardPageCount(int btype) {
+		int result = 0;
+		BoardDAO dao = BoardDAO.getInstance();
+		result = dao.getBoardPageCount(btype);
+		return result;
+	}
+	
+	public ArrayList<BoardVO> getBoardList(int btype) {
+		ArrayList<BoardVO> result = null;
+		BoardDAO dao = BoardDAO.getInstance();
+		result = dao.getBoardList(btype);
+		
+		return result;
+	}
+
+}
